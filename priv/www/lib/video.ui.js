07bfad8e7f6b8b15462ef0dd939c984acd1b53be
François de Metz <fdemetz@af83.com>
2010-11-17 13:59:40 +0100

    improved video widget and use it on all live meetings
    * new publish webcam widget
    * allow to pass stream options

--- priv/www/lib/video.ui.js
+++ priv/www/lib/video.ui.js
@@ -3,17 +3,21 @@ $.widget("uce.video", {
     // Default options
     options: {
         domain : "localhost",
+        stream : "ucengine",
         width  : 610,
         height : 415
     },
     _getFlashSrc: function() {
         if (this._publish)
-            return '/publish_video.swf?stream=test_stream%20&streamtype=live&server=rtmp://' + this.options.domain + '/encrev1/room1&token=token1';
+            return '/publish_video.swf';
         else
-            return '/receive_video.swf?stream=test_stream%20&streamtype=live&server=rtmp://' + this.options.domain + '/encrev1/room1&token=token1';
+            return '/receive_video.swf';
     },
     _getFlashVar: function() {
-        return "stream=test_stream&streamtype=live&server=rtmp://" + this.options.domain + "/encrev1/room1&width="+ this.options.width  +"&height="+ this.options.height +"&token=token1";
+        if (this._publish)
+            return "stream="+ this.options.stream +"&server=rtmp://" + this.options.domain;
+        else
+            return "stream="+ this.options.stream +"&streamtype=live&server=rtmp://" + this.options.domain +"&token=p&width="+ this.options.width  +"&height="+ this.options.height;
     },
     _create: function() {
         this.element.addClass('ui-widget ui-video');
