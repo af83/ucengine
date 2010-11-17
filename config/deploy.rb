c799f9c6d70f4edd118da95965c8837a95fb416a
phorque <victor.goya@af83.com>
2010-11-17 15:27:24 +0100

    Update deploy.rb file to use the new git repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_via, :remote_cache
 desc "Local Environment"
 task :env_local do
   set :branch, 'master'
-  set :repository,  "af83@git.af83.com:encre-proto.git"
+  set :repository,  "git@github.com:AF83/ucengine.git"
   set :application, "localhost"
   set :application_type, "development"
   set :deploy_to, "/home/encre"
@@ -18,33 +18,33 @@ end
 desc "Test Environment"
 task :env_test do
   set :branch, 'master'
-  set :repository,  "af83@git.af83.com:encre-proto.git"
+  set :repository,  "git@github.com:AF83/ucengine.git"
   set :application, "encre-test.af83.com"
   set :application_type, "development"
-  set :deploy_to, "/var/www/encre/encre-proto"
+  set :deploy_to, "/var/www/encre/ucengine"
   set :config_file, "test.cfg"
   server "#{user}@#{application}", :app, :web, :db, :primary => true
-  set :default_environment, {"ERL_LIBS" => "/var/www/encre/lib/erlang/lib"}
+  set :default_environment, {"ERL_LIBS" => "/var/www/ucengine/lib/erlang/lib"}
 end
 
 namespace :deploy do
-  desc "Start the encre server"
+  desc "Start the ucengine server"
   task :start do
-    run "make start -C #{current_path}/encre"
+    run "make start -C #{current_path}"
   end
-  desc "Stop the encre server"
+  desc "Stop the ucengine server"
   task :stop do
-    run "if [ -d #{current_path} ]; then make stop -C #{current_path}/encre; fi"
+    run "if [ -d #{current_path} ]; then make stop -C #{current_path}; fi"
   end
-  desc "Restart the encre server"
+  desc "Restart the ucengine server"
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "make restart -C #{current_path}/encre"
+    run "make restart -C #{current_path}"
   end
 
   desc "Copy the configuration file"
   task :config do
-    config = "#{current_path}/encre/config/#{config_file}"
-    run "cp #{config} #{current_path}/encre/etc/encre.cfg"
+    config = "#{current_path}/config/#{config_file}"
+    run "cp #{config} #{current_path}/etc/uce.cfg"
   end
 end
 
