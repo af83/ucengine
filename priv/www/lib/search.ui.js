893a9fd942378bd5134b7a6c992b8037d1fa89cc
François de Metz <fdemetz@af83.com>
2010-11-19 15:34:11 +0100

    removed unwanted stuff in search widget

--- priv/www/lib/search.ui.js
+++ priv/www/lib/search.ui.js
@@ -1,35 +1,24 @@
 $.widget("uce.search", {
     options: {
-        ucemeeting : null
+        ucemeeting  : null,
+        typevent    : "twitter.tweet.new",
+        placeholder : "Search"
     },
     _create: function() {
         this.element.addClass('ui-widget ui-search');
         this.form = $('<form>').attr('method', 'post');
-        $('<input>').attr({'type': 'text', 'name': 'keywords', 'placeholder': 'Search'}).appendTo(this.form);
+        $('<input>').attr({'type': 'text', 'name': 'keywords', 'placeholder': this.options.placeholder}).appendTo(this.form);
         $('<input>').attr({'type': 'submit', 'value': 'ok'}).appendTo(this.form);
         this.form.appendTo(this.element);
-        var toggle = $('<div>').attr({'class': 'ui-search-toggle'});
-        toggle.appendTo(this.form);
-        this.toggle = toggle;
-        var that = this;
-        toggle.bind('click', function() {
-            if ($('#search-results')[0].style.display == 'none') {
-                $('#search-results')[0].style.display = 'block';
-                that.toggle.text("↓");
-            } else {
-                $('#search-results')[0].style.display = 'none';
-                that.toggle.text("↑");
-            }
-        });
-        var that = this;
+
+        var meeting = this.options.ucemeeting;
+        var type    = this.options.typeevent;
         this.form.bind('submit', function() {
             var keywords = this.elements['keywords'].value;
-            that.options.ucemeeting.getEvents({'search': keywords, 'type': 'twitter.tweet.new'}, function(err, events) {
-                that.options.ucemeeting.trigger({'type': 'internal.search.result',
-                                                 'from': 'internal',
-                                                 'metadata': {'events': events}});
-                $('#search-results')[0].style.display='block';
-                that.toggle.text("↓");
+            meeting.getEvents({'search': keywords, 'type': type}, function(err, events) {
+                meeting.trigger({'type': 'internal.search.result',
+                                 'from': 'internal',
+                                 'metadata': {'events': events}});
             });
             return false;
         });
