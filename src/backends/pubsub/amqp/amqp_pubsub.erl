bf9edd770cf9c12f698c22d08a546dc7c58703f9
phorque <victor.goya@af83.com>
2010-12-07 14:37:40 +0100

    Add the missing code for a fully working AMQP backend

--- src/backends/pubsub/amqp/amqp_pubsub.erl
+++ src/backends/pubsub/amqp/amqp_pubsub.erl
@@ -9,6 +9,7 @@
 	 publish/4,
 	 subscribe/9,
 	 unsubscribe/1,
+	 handle_messages/3,
 	 handle_call/3,
 	 handle_cast/2,
 	 handle_info/2,
@@ -24,73 +25,97 @@ start_link() ->
 init([]) ->
     case utils:get(config:get(amqp), [username, password, host, port]) of
 	[Username, Password, Host, Port] ->
-	    io:format("PLOP~n"),
-	    Connection = amqp_connection:start(network, #amqp_params{username=list_to_binary(Username),
-								     password=list_to_binary(Password),
-								     host=Host,
-								     port=Port}),
-	    io:format("CONN ~p~n", [Connection]),
-	    Channel = amqp_connection:open_channel(Connection),
-	    io:format("CHAN: ~p~n", [Channel]),
+	    {ok, Connection} =
+		amqp_connection:start(network, #amqp_params{username=list_to_binary(Username),
+							    password=list_to_binary(Password),
+							    host=Host,
+							    port=Port}),
+
+	    {ok, Channel} = amqp_connection:open_channel(Connection),
 	    {ok, {Channel}};
 	_ ->
 	    {error, bad_configuration}
     end.
 
-publish(Location, Type, From, Message) ->
-    gen_server:call(?MODULE, {publish, Location, Type, From, Message}).
+publish(Location, Type, From, Id) ->
+    case Location of
+	["", ""] ->
+	    gen_server:call(?MODULE, {publish, Location, Type, From, Id}),
+	    gen_server:call(?MODULE, {publish, Location, '_', From, Id});
+	[Org, ""] ->
+	    gen_server:call(?MODULE, {publish, Location, Type, From, Id}),
+	    gen_server:call(?MODULE, {publish, Location, '_', From, Id}),
+	    gen_server:call(?MODULE, {publish, ["", ""], Type, From, Id}),
+	    gen_server:call(?MODULE, {publish, ["", ""], '_', From, Id});
+	[Org, Meeting] ->
+	    gen_server:call(?MODULE, {publish, Location, Type, From, Id}),
+	    gen_server:call(?MODULE, {publish, Location, '_', From, Id}),
+	    gen_server:call(?MODULE, {publish, [Org, ""], Type, From, Id}),
+	    gen_server:call(?MODULE, {publish, [Org, ""], '_', From, Id}),
+	    gen_server:call(?MODULE, {publish, ["", ""], Type, From, Id}),
+	    gen_server:call(?MODULE, {publish, ["", ""], '_', From, Id})
+    end.
 
 subscribe(Pid, Location, Search, From, Types, Uid, _Start, _End, _Parent) ->
-    lists:foreach(fun(Type) ->
-			  case Location of
-			      [Org, Meeting] ->
-				  gen_server:cast(?MODULE,
-						  {subscribe, [Org, Meeting], Uid, Search, Type, From, Pid}),
-				  gen_server:cast(?MODULE,
-						  {subscribe, [Org], Uid, Search, Type, From, Pid}),
-				  gen_server:cast(?MODULE,
-						  {subscribe, [], Uid, Search, Type, From, Pid});
-			      [Org] ->
-				  gen_server:cast(?MODULE,
-						  {subscribe, [Org], Uid, Search, Type, From, Pid}),
-				  gen_server:cast(?MODULE,
-						  {subscribe, [], Uid, Search, Type, From, Pid});
-			      [] ->
-				  gen_server:cast(?MODULE,
-						  {subscribe, [], Uid, Search, Type, From, Pid})
-			  end
-
-		  end,
-		  Types).
+    [gen_server:cast(?MODULE, {subscribe, Location, Uid, Search, Type, From, Pid}) || Type <- Types].
 
 unsubscribe(Pid) ->
     gen_server:cast(?MODULE, {unsubscribe, Pid}).
 
-params_to_queue(Location, Search, Type, From) ->
-    QueueLocation = case Location of
-			[Org, Meeting] ->
-			    yaws_api:url_encode(Org) ++ "," ++ yaws_api:url_encode(Meeting);
-			[Org] ->
-			    yaws_api:url_encode(Org);
-			[] ->
-			    ""
-		    end,
-    QueueType = yaws_api:url_encode(Type),
-    QueueLocation ++ "|" ++ QueueType.
+params_to_queue([Org, Meeting], Type) ->
+    QueueLocation = yaws_api:url_encode(Org) ++ "," ++ yaws_api:url_encode(Meeting),
+    QueueType = case Type of
+		    '_' ->
+			[];
+		    _ ->
+			yaws_api:url_encode(Type)
+		end,
+    list_to_binary(QueueLocation ++ "|" ++ QueueType).
 
-handle_call({publish, Location, Type, From, Message}, _From, {Channel} = State) ->
-    io:format("PLOP~n"),
+handle_messages(Channel, Queue, Pid) ->
+    receive
+	#'basic.consume_ok'{} ->
+	    ?MODULE:handle_messages(Channel, Queue, Pid);
+	#'basic.cancel_ok'{} ->
+	    ok;
+	{#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{payload=Id}} ->
+	    amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
+	    Pid ! {message, Id}
+    end.
+
+handle_call({publish, Location, Type, From, Id}, _From, {Channel} = State) ->
+    Queue = params_to_queue(Location, Type),
+
+    Exchange = <<>>,
+    Publish = #'basic.publish'{exchange = Exchange, routing_key = Queue},
+    amqp_channel:cast(Channel, Publish, #amqp_msg{payload = list_to_binary(Id)}),
     {reply, ok, State}.
 
 handle_cast({subscribe, Location, Uid, Search, Type, From, Pid}, {Channel} = State) ->
-    Sub = #'basic.consume'{queue = params_to_queue(Location, Search, Type, From)},
-    Pid = spawn(fun() ->
-			nothing
-		end),
-    #'basic.consume_ok'{consumer_tag = Tag} = amqp_channel:subscribe(Channel, Sub, Pid),
+    Queue = params_to_queue(Location, Type),
+
+    % Create queue
+    Declare = #'queue.declare'{queue = Queue},
+    #'queue.declare_ok'{} = amqp_channel:call(Channel, Declare),
+
+    % Subscribe to it
+    Sub = #'basic.consume'{queue = Queue},
+    Listener = spawn_link(?MODULE, handle_messages, [Channel, Queue, Pid]),
+    case catch amqp_channel:subscribe(Channel, Sub, Listener) of
+	#'basic.consume_ok'{consumer_tag = Tag} ->
+	    put(Pid, Tag);
+	Error ->
+	    ?ERROR_MSG("~p: ~p~n", [?MODULE, Error])	    
+    end,
     {noreply, State};
 
-handle_cast({unsubscribe, Location, Uid, Search, Type, From, Pid}, {Channel} = State) ->
+handle_cast({unsubscribe, Pid}, {Channel} = State) ->
+    case get(Pid) of
+	undefined ->
+	    nothing;
+	Tag ->
+	    amqp_channel:call(Channel, #'basic.cancel'{consumer_tag = Tag})
+    end,
     {noreply, State}.
 
 code_change(_,State,_) ->
