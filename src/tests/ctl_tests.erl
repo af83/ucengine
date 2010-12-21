fc9a771e31b4524cb0d2c99c57ef9f5d9d10a05f
Romain Gauthier <romain.gauthier@af83.com>
2010-12-21 18:46:35 +0100

    ctl_tests: Test the user actions.

--- src/tests/ctl_tests.erl
+++ src/tests/ctl_tests.erl
@@ -47,6 +47,27 @@ ctl_meeting_test_() ->
       end
     }.
 
+ctl_user_test_() ->
+    { setup
+      , fun fixtures:setup/0
+      , fun fixtures:teardown/1
+      , fun(_Testers) ->
+        [ ?_test(test_user_add())
+        , ?_test(test_user_add_missing_parameter())
+        , ?_test(test_user_get())
+        , ?_test(test_user_get_missing_parameter())
+        , ?_test(test_user_get_not_found())
+        , ?_test(test_user_update())
+        , ?_test(test_user_update_missing_parameter())
+        , ?_test(test_user_update_not_found())
+        , ?_test(test_user_delete())
+        , ?_test(test_user_delete_missing_parameter())
+        , ?_test(test_user_delete_not_found())
+        , ?_test(test_user_list())
+        ]
+      end
+    }.
+
 
 %%
 %% Org
@@ -170,4 +191,83 @@ test_meeting_list_missing_parameter() ->
 test_meeting_list_not_found() ->
     Params = [{"org", ["org that doesn't exitst"]}, {"status", ["all"]}],
     error = uce_ctl:action(meeting, list, []).
+
+%%
+%% User
+%%
+
+test_user_add() ->
+    {error, not_found} = uce_user:get("test.user@af83.com"),
+    Params = [ {"uid", ["test.user@af83.com"]}
+             , {"auth", ["password"]}
+             , {"credential", ["pwd"]}
+             ],
+    ok = uce_ctl:action(user, add, Params),
+    {ok, #uce_user{uid="test.user@af83.com",
+                   auth="password",
+                   credential="pwd"}} = uce_user:get("test.user@af83.com").
+test_user_add_missing_parameter() ->
+    Params = [ {"auth", ["password"]}
+             , {"credential", ["pwd"]}
+             ],
+    error = uce_ctl:action(user, add, Params).
+
+test_user_get() ->
+    Params = [ {"uid", ["participant.user@af83.com"]}
+             , {"auth", ["password"]}
+             , {"credential", ["pwd"]}
+             ],
+    ok = uce_ctl:action(user, get, Params).
+test_user_get_missing_parameter() ->
+    Params = [{"auth", ["password"]}, {"credential", ["pwd"]}],
+    error = uce_ctl:action(user, get, Params).
+test_user_get_not_found() ->
+    Params = [ {"uid", ["nobody@af83.com"]}
+             , {"auth", ["password"]}
+             , {"credential", ["pwd"]}
+             ],
+    error = uce_ctl:action(user, get, Params).
+
+test_user_update() ->
+    {ok, #uce_user{uid="anonymous.user@af83.com",
+                   auth="anonymous",
+                   credential=""}} =
+        uce_user:get("anonymous.user@af83.com"),
+    Params = [ {"uid", ["anonymous.user@af83.com"]}
+             , {"auth", ["password"]}
+             , {"credential", ["pwd"]}
+             ],
+    ok = uce_ctl:action(user, update, Params),
+    {ok, #uce_user{uid="anonymous.user@af83.com",
+                   auth="password",
+                   credential="pwd"}} =
+        uce_user:get("anonymous.user@af83.com").
+test_user_update_missing_parameter() ->
+    error = uce_ctl:action(org, update, []).
+test_user_update_not_found() ->
+    Params = [ {"uid", ["nobody@af83.com"]}
+             , {"auth", ["password"]}
+             , {"credential", ["passwd"]}
+             ],
+    error = uce_ctl:action(user, update, Params).
+
+test_user_delete() ->
+    {ok, #uce_user{uid="participant.user@af83.com",
+                   auth="password",
+                   credential="pwd"}} = uce_user:get("participant.user@af83.com"),
+    Params = [{"uid", ["participant.user@af83.com"]}],
+    ok = uce_ctl:action(user, delete, Params),
+    {error, not_found} = uce_user:get("participant.user@af83.com").
+test_user_delete_missing_parameter() ->
+    error = uce_ctl:action(user, delete, []).
+test_user_delete_not_found() ->
+    Params = [ {"uid", ["nobody@af83.com"]}
+             , {"auth", ["password"]}
+             , {"credential", ["passwd"]}
+             ],
+    error = uce_ctl:action(user, delete, Params).
+
+
+test_user_list() ->
+    ok = uce_ctl:action(user, list, []).
 
