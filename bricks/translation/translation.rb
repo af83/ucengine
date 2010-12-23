003168b7c6a147e22a0011f316f00abf93def278
François de Metz <fdemetz@af83.com>
2010-12-23 14:49:31 +0100

    fixed translation bricks without orgs

--- bricks/translation/translation.rb
+++ bricks/translation/translation.rb
@@ -12,7 +12,7 @@ Daemons.run_proc('translation') do
     languages = ["fr", "en", "it"]
     UCEngine.new("localhost", 5280, UCEngine::DEBUG).connect("translation",
                                             :credential => "d713ab03c0280f82709f865ffa2240a38c26f09b") do |uce|
-      uce.subscribe(["af83", "demo"], :type => "chat.message.new", :start => uce.time) do |event|
+      uce.subscribe(["demo"], :type => "chat.message.new", :start => uce.time) do |event|
         if event['metadata']['lang'] and event['metadata']['text']
           languages.each do |language|
             next if language == event['metadata']['lang']
