02b7ff0784437a1dec225bcaca6fe6b33fa4b156
François de Metz <fdemetz@af83.com>
2011-01-07 15:42:56 +0100

    added spec on document brick
    Signed-off-by: François de Metz <fdemetz@af83.com>
    Signed-off-by: Laurent Arnoud <larnoud@af83.com>

---
+++ bricks/document/lib/ucengine_document.rb
@@ -0,0 +1,30 @@
+require 'RMagick'
+
+module UCEngineDocument
+  def handle_upload_event(uce, event)
+    if event['metadata']['mime'] == 'application/pdf'
+      content = uce.download(event['meeting'], event['metadata']['id'])
+      file = Tempfile.new(["content-", ".pdf"])
+      file.write(content)
+      file.flush()
+      images = Hash.new
+      begin
+        Magick::ImageList.new(file.path).each_with_index do |image, i|
+          tmp_image = Tempfile.new(["content-", ".jpg"])
+          image.write(tmp_image.path)
+          result = uce.upload(event['meeting'], tmp_image)
+          tmp_image.close!
+          images[i] = result['result']
+        end
+        uce.publish(:location => [event['meeting']],
+                    :parent => event['id'],
+                    :from => 'document',
+                    :type => 'document.conversion.done',
+                    :metadata => images)
+      rescue => error
+        puts "Fatal error: #{error}"
+      end
+      file.close!
+    end
+  end
+end
---
+++ bricks/document/spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require '/home/francois/dev/ucengine/ucengine.rb/lib/ucengine'
+
+require 'lib/ucengine_document'
