556b5de081fac09189e3750341b2898408a786e4
Romain Gauthier <romain.gauthier@af83.com>
2011-02-01 15:51:04 +0100

    timer: delete-trailing-whitespace and untabify.

--- priv/www/lib/timer/timer.ui.js
+++ priv/www/lib/timer/timer.ui.js
@@ -1,38 +1,38 @@
 $.uce.widget("timer", {
     options: {
         ucemeeting : null,
-	    start	   : 0,
-	    state	   : 'elapsed'
+        start      : 0,
+        state      : 'elapsed'
     },
 
     _create: function() {
-	var that = this;
+        var that = this;
 
-	this.element.addClass('ui-widget ui-timer');
-	this._content = $('<div>')
-	        .attr('class', 'ui-widget-content')
-	        .appendTo(this.element)
-	        .click(function() {
+        this.element.addClass('ui-widget ui-timer');
+        this._content = $('<div>')
+            .attr('class', 'ui-widget-content')
+            .appendTo(this.element)
+            .click(function() {
                 that.toggleState();
             });
 
-	var states = [];
-	this.options.ucemeeting.get(function(err, meeting, xhr) {
-	    that._elapsed = that.options.start;
-        that._meetingStart = meeting.start_date;
-        that._meetingEnd = meeting.end_date;
-	    that._handleTimeout(that);
-	});
+        var states = [];
+        this.options.ucemeeting.get(function(err, meeting, xhr) {
+            that._elapsed = that.options.start;
+            that._meetingStart = meeting.start_date;
+            that._meetingEnd = meeting.end_date;
+            that._handleTimeout(that);
+        });
     },
 
     clear: function() {
     },
 
     _handleTimeout: function(that) {
-	    var stateText = "";
+        var stateText = "";
 
         if (that._elapsed > that._meetingEnd) {
-            that._elapsed = that._meetingEnd 
+            that._elapsed = that._meetingEnd;
         }
 
         var elapsed = that._elapsed - that._meetingStart;
@@ -48,47 +48,47 @@ $.uce.widget("timer", {
             valueText += " / \u221E"
         }
 
-	    var value = $('<span>')
-	        .attr('class', 'ui-timer-value')
-	        .text(valueText);
+        var value = $('<span>')
+            .attr('class', 'ui-timer-value')
+            .text(valueText);
 
-	    that._elapsed += 1000;
+        that._elapsed += 1000;
 
-	    that._content.empty();
-	    that._content.append(value);
-	    that._timeout = setTimeout(function() {
-	        that._handleTimeout(that);
-	    }, 1000);
+        that._content.empty();
+        that._content.append(value);
+        that._timeout = setTimeout(function() {
+            that._handleTimeout(that);
+        }, 1000);
     },
-    
+
     _format: function(timestamp) {
         var neg = false;
         if (timestamp < 0) {
             timestamp *= -1;
             neg = true;
         }
-        
-	    var date = new Date(timestamp);
-        
-	    var hours = date.getHours() - 1;
-	    if (hours < 10) {
-	        hours = "0" + hours;
-	    }
-	    var minutes = date.getMinutes();
-	    if (minutes < 10) {
-	        minutes = "0" + minutes;
-	    }
-	    var seconds = date.getSeconds();
-	    if (seconds < 10) {
-	        seconds = "0" + seconds;
-	    }
-        
-	    var valueText = hours + ":" + minutes + ":" + seconds;
-        
+
+        var date = new Date(timestamp);
+
+        var hours = date.getHours() - 1;
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        var minutes = date.getMinutes();
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        var seconds = date.getSeconds();
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        var valueText = hours + ":" + minutes + ":" + seconds;
+
         if (neg) {
             valueText = "-" + valueText;
         }
-        
+
         return (valueText);
     },
 
