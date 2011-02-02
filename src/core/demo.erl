cb0de2b01ced5c0562f9c3d1f3f3f3e4d2abd36d
François de Metz fdemetz@af83.com <fdemetz@af83.com>
2011-02-02 19:44:31 +0100

    untabify and reindent demo.erl.

--- src/core/demo.erl
+++ src/core/demo.erl
@@ -7,46 +7,45 @@
 
 start() ->
     uce_infos:update([{"description", "af83media specializes in digital communication. Our mission is to design and manage online content and communities."},
-                   {"logo", "af83.png"},
-                   {"htags", "af83"}]),
+                      {"logo", "af83.png"},
+                      {"htags", "af83"}]),
 
     uce_meeting:add(#uce_meeting{id=["demo"],
-				     metadata=[{"description", "UCEngine demo meetup"},
-					       {"video", "http://localhost/dev/ucengine/test"}],
-				     start_date=utils:now(),
-				     end_date=?NEVER_ENDING_MEETING}),
+                                 metadata=[{"description", "U.C.Engine demo meetup"}],
+                                 start_date=utils:now(),
+                                 end_date=?NEVER_ENDING_MEETING}),
     uce_event:add(#uce_event{type="twitter.hashtag.add",
-			     location=["demo"],
-			     from="ucengine",
-			     metadata=[{"hashtag", "#TED"}]}),
+                             location=["demo"],
+                             from="ucengine",
+                             metadata=[{"hashtag", "#TED"}]}),
     uce_event:add(#uce_event{type="twitter.hashtag.add",
-			     location=["demo"],
-			     from="ucengine",
-			     metadata=[{"hashtag", "#sinek"}]}),
+                             location=["demo"],
+                             from="ucengine",
+                             metadata=[{"hashtag", "#sinek"}]}),
     uce_event:add(#uce_event{type="twitter.hashtag.add",
-			     location=["demo"],
-			     from="ucengine",
-			     metadata=[{"hashtag", "#simonsinek"}]}),
+                             location=["demo"],
+                             from="ucengine",
+                             metadata=[{"hashtag", "#simonsinek"}]}),
     uce_event:add(#uce_event{type="twitter.hashtag.add",
-			     location=["demo"],
-			     from="ucengine",
-			     metadata=[{"hashtag", "#ucengine"}]}),
+                             location=["demo"],
+                             from="ucengine",
+                             metadata=[{"hashtag", "#ucengine"}]}),
     uce_event:add(#uce_event{type="twitter.hashtag.add",
-			     location=["demo"],
-			     from="ucengine",
-			     metadata=[{"hashtag", "#ucengine"}]}),
+                             location=["demo"],
+                             from="ucengine",
+                             metadata=[{"hashtag", "#ucengine"}]}),
 
     uce_meeting:add(#uce_meeting{id=["demo2"],
-				     metadata=[{"description", "Meeting R&D"},
-					       {"video", "/test"}],
-				     start_date=utils:now(),
-				     end_date=?NEVER_ENDING_MEETING}),
+                                 metadata=[{"description", "Meeting R&D"},
+                                           {"video", "/test"}],
+                                 start_date=utils:now(),
+                                 end_date=?NEVER_ENDING_MEETING}),
     uce_meeting:add(#uce_meeting{id=["agoroom"],
-				     metadata=[{"description", "Meeting agoroom"},
-					       {"video", "http://localhost/dev/ucengine/test"}],
-				     start_date=1287738533649,
-				     end_date=1287739733649}),
-    
+                                 metadata=[{"description", "Meeting agoroom"},
+                                           {"video", "http://encre.2metz.fr/simonsinek_2009x"}],
+                                 start_date=1287738533649,
+                                 end_date=1287739733649}),
+
     user_controller:add(["thierry.bomandouki@af83.com"], ["password", "pwd", []], []),
     user_controller:add(["victor.goya@af83.com"], ["password", "pwd", []], []),
     user_controller:add(["louis.ameline@af83.com"], ["password", "pwd", []], []),
@@ -57,13 +56,13 @@ start() ->
     ok = feed(),
 
     case utils:get(config:get(admin), [uid, auth]) of
-	[Uid, Auth] ->
-	    {ok, Sid} = uce_presence:add(#uce_presence{uid=Uid, 
-						       auth=Auth,
-						       metadata=[]}),
-	    io:format("Admin: ~p/~p~n", [Uid, Sid]);
-	Reason ->
-	    io:format("No admin account (~p)~n", [Reason])
+        [Uid, Auth] ->
+            {ok, Sid} = uce_presence:add(#uce_presence{uid=Uid,
+                                                       auth=Auth,
+                                                       metadata=[]}),
+            io:format("Admin: ~p/~p~n", [Uid, Sid]);
+        Reason ->
+            io:format("No admin account (~p)~n", [Reason])
     end,
     ok.
 
@@ -78,4 +77,3 @@ feed([Path|Paths]) ->
 feed() ->
     Paths = filelib:wildcard("config/samples/*/*.json"),
     feed(Paths).
-
