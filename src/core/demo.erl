387b575e54c6ff6bc0da32fab816e40f23d6c378
François de Metz fdemetz@af83.com <fdemetz@af83.com>
2011-02-03 17:51:25 +0100

    Update default demo text.

--- src/core/demo.erl
+++ src/core/demo.erl
@@ -23,7 +23,7 @@
 
 
 start() ->
-    uce_infos:update([{"description", "af83media specializes in digital communication. Our mission is to design and manage online content and communities."},
+    uce_infos:update([{"description", "U.C.Engine is a publish/subscribe server with persistence. It allows you to build real time applications like collaboration based services, live meetings, games or anything that fits well in an event driven philosophy."},
                       {"logo", "af83.png"},
                       {"htags", "af83"}]),
 
