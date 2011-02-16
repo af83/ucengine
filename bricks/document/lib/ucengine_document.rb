9788939fc8ca17e8998504afff2cbe46d33c7ac3
Victor Goya <victor.goya@af83.com>
2011-02-16 14:56:04 +0100

    bricks: use the location parameter instead of meeting

--- bricks/document/lib/ucengine_document.rb
+++ bricks/document/lib/ucengine_document.rb
@@ -3,7 +3,7 @@ require 'RMagick'
 module UCEngineDocument
   def handle_upload_event(uce, event)
     if event['metadata']['mime'] == 'application/pdf'
-      content = uce.download(event['meeting'], event['metadata']['id'])
+      content = uce.download(event['location'], event['metadata']['id'])
       file = Tempfile.new(["content-", ".pdf"])
       file.write(content)
       file.flush()
@@ -12,11 +12,11 @@ module UCEngineDocument
         Magick::ImageList.new(file.path).each_with_index do |image, i|
           tmp_image = Tempfile.new(["content-", ".jpg"])
           image.write(tmp_image.path)
-          result = uce.upload(event['meeting'], tmp_image)
+          result = uce.upload(event['location'], tmp_image)
           tmp_image.close!
           images[i] = result['result']
         end
-        uce.publish(:location => [event['meeting']],
+        uce.publish(:location => event['location'],
                     :parent => event['id'],
                     :from => 'document',
                     :type => 'document.conversion.done',
