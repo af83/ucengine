a277fc5610037fe69725ea57b0b81d15775c6145
François de Metz fdemetz@af83.com <fdemetz@af83.com>
2011-02-21 11:42:33 +0100

    Untabify.

--- src/backends/db/mongodb/mongodb_db.erl
+++ src/backends/db/mongodb/mongodb_db.erl
@@ -28,14 +28,14 @@
 
 init({Pool, MongoPoolInfos}) ->
     case utils:get(MongoPoolInfos,
-		   [size, host, port, database],
-		   [1, "localhost", ?DEFAULT_MONGODB_PORT, ?DEFAULT_MONGODB_NAME]) of
-	[Size, Host, Port, Name] ->
-	    application:start(emongo),
-	    emongo:add_pool(Pool, Host, Port, Name, Size),
-	    ok;
-	_ ->
-	    {error, bad_configuration}
+                   [size, host, port, database],
+                   [1, "localhost", ?DEFAULT_MONGODB_PORT, ?DEFAULT_MONGODB_NAME]) of
+        [Size, Host, Port, Name] ->
+            application:start(emongo),
+            emongo:add_pool(Pool, Host, Port, Name, Size),
+            ok;
+        _ ->
+            {error, bad_configuration}
     end.
 
 drop() ->
