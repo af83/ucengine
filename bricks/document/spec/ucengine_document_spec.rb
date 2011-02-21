410bd25d4071a76b5f0dabcd7325e2a6ae29106a
Victor Goya <victor.goya@af83.com>
2011-02-21 12:02:53 +0100

    document: fix tests

--- bricks/document/spec/ucengine_document_spec.rb
+++ bricks/document/spec/ucengine_document_spec.rb
@@ -7,7 +7,7 @@ describe UCEngineDocument do
   end
 
   it "should not transform non pdf document" do
-    UCEngineDocument::handle_upload_event(@uce, {"meeting" => "testmeeting",
+    UCEngineDocument::handle_upload_event(@uce, {"location" => "testmeeting",
                                                  "id" => "id",
                                                  "metadata" => {"id" => "small_12345",
                                                                 "mime" => "text/plain"}})
@@ -17,13 +17,13 @@ describe UCEngineDocument do
     value = File.read('./spec/samples/small.pdf')
     @uce.should_receive(:download).once.with('testmeeting', 'small_12345').and_return(value)
     @uce.should_receive(:upload).once.with('testmeeting', an_instance_of(Tempfile)).and_return({"result" => "id1"})
-    @uce.should_receive(:publish).once.with(hash_including(:location => ["testmeeting"],
+    @uce.should_receive(:publish).once.with(hash_including(:location => "testmeeting",
                                                            :parent   => "id",
                                                            :from     => "document",
                                                            :type     => "document.conversion.done",
                                                            :metadata => {0 => "id1"}))
 
-    UCEngineDocument::handle_upload_event(@uce, {"meeting" => "testmeeting",
+    UCEngineDocument::handle_upload_event(@uce, {"location" => "testmeeting",
                                                  "id" => "id",
                                                  "metadata" => {"id" => "small_12345",
                                                                 "mime" => "application/pdf"}})
@@ -33,12 +33,12 @@ describe UCEngineDocument do
     value = File.read('./spec/samples/big.pdf')
     @uce.should_receive(:download).once.with('testmeeting', 'small_12345').and_return(value)
     @uce.should_receive(:upload).exactly(9).times.with('testmeeting', an_instance_of(Tempfile)).and_return({"result" => "id1"})
-    @uce.should_receive(:publish).once.with(hash_including(:location => ["testmeeting"],
+    @uce.should_receive(:publish).once.with(hash_including(:location => "testmeeting",
                                                            :parent   => "id",
                                                            :from     => "document",
                                                            :type     => "document.conversion.done"))
 
-    UCEngineDocument::handle_upload_event(@uce, {"meeting" => "testmeeting",
+    UCEngineDocument::handle_upload_event(@uce, {"location" => "testmeeting",
                                                  "id" => "id",
                                                  "metadata" => {"id" => "small_12345",
                                                                 "mime" => "application/pdf"}})
@@ -50,7 +50,7 @@ describe UCEngineDocument do
     @uce.should_not_receive(:upload).with(any_args())
     @uce.should_not_receive(:publish).with(any_args())
 
-    UCEngineDocument::handle_upload_event(@uce, {"meeting" => "testmeeting",
+    UCEngineDocument::handle_upload_event(@uce, {"location" => "testmeeting",
                                                  "id" => "id",
                                                  "metadata" => {"id" => "small_12345",
                                                  "mime" => "application/pdf"}})
