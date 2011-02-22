e0de4473a9b273e30e144ac2838a793ffe356e7a
François de Metz fdemetz@af83.com <fdemetz@af83.com>
2011-02-22 14:59:13 +0100

    Refacto demo.erl.
    Signed-off-by: François de Metz <fdemetz@af83.com>
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- src/core/demo.erl
+++ src/core/demo.erl
@@ -23,121 +23,49 @@
 
 
 start() ->
-
     Domain = config:get(default_domain),
 
     uce_infos:update(#uce_infos{domain=Domain,
                                 metadata=[{"description", "U.C.Engine is a publish/subscribe server with persistence. It allows you to build real time applications like collaboration based services, live meetings, games or anything that fits well in an event driven philosophy."},
-                                         {"logo", "ucengine.png"},
-                                         {"htags", "ucengine"}]}),
+                                          {"logo", "ucengine.png"},
+                                          {"htags", "ucengine"}]}),
 
     catch uce_meeting:add(#uce_meeting{id={"demo", Domain},
                                        metadata=[{"description", "U.C.Engine demo meetup"}],
                                        start_date=utils:now(),
                                        end_date=?NEVER_ENDING_MEETING}),
 
-    catch uce_user:add(#uce_user{id={"thierry.bomandouki@af83.com", Domain},
-                                 auth="password",
-                                 credential="pwd",
-                                 metadata=[]}),
-    uce_acl:add(#uce_acl{user={"thierry.bomandouki@af83.com", Domain},
-                         action="add",
-                         object="presence"}),
-
-    catch uce_user:add(#uce_user{id={"victor.goya@af83.com", Domain},
-                                 auth="password",
-                                 credential="pwd",
-                                 metadata=[]}),
-    uce_acl:add(#uce_acl{user={"victor.goya@af83.com", Domain},
-                         action="add",
-                         object="presence"}),
-
-    catch uce_user:add(#uce_user{id={"louis.ameline@af83.com", Domain},
-                                 auth="password",
-                                 credential="pwd",
-                                 metadata=[]}),
-    uce_acl:add(#uce_acl{user={"louis.ameline@af83.com", Domain},
-                         action="add",
-                         object="presence"}),
-
-    catch uce_user:add(#uce_user{id={"alexandre.eisenchteter@af83.com", Domain},
-                                 auth="password",
-                                 credential="pwd",
-                                 metadata=[]}),
-    uce_acl:add(#uce_acl{user={"alexandre.eisenchteter@af83.com", Domain},
-                         action="add",
-                         object="presence"}),
-    
-    catch uce_user:add(#uce_user{id={"romain.gauthier@af83.com", Domain},
-                                 auth="password",
-                                 credential="pwd",
-                                 metadata=[]}),
-    uce_acl:add(#uce_acl{user={"romain.gauthier@af83.com", Domain},
-                         action="add",
-                         object="presence"}),
-
-    catch uce_user:add(#uce_user{id={"participant", Domain},
-                                 auth="password",
-                                 credential="pwd",
-                                 metadata=[]}),
-    uce_acl:add(#uce_acl{user={"participant", Domain},
-                         action="add",
-                         object="presence"}),
-    
-    uce_event:add(#uce_event{domain=Domain,
-                             type="twitter.hashtag.add",
-                             location={"demo", Domain},
-                             from={"participant", Domain},
-                             metadata=[{"hashtag", "#TED"}]}),
-    uce_event:add(#uce_event{domain=Domain,
-                             type="twitter.hashtag.add",
-                             location={"demo", Domain},
-                             from={"participant", Domain},
-                             metadata=[{"hashtag", "#sinek"}]}),
-    uce_event:add(#uce_event{domain=Domain,
-                             type="twitter.hashtag.add",
-                             location={"demo", Domain},
-                             from={"participant", Domain},
-                             metadata=[{"hashtag", "#simonsinek"}]}),
-    uce_event:add(#uce_event{domain=Domain,
-                             type="twitter.hashtag.add",
-                             location={"demo", Domain},
-                             from={"participant", Domain},
-                             metadata=[{"hashtag", "#ucengine"}]}),
-
     catch uce_meeting:add(#uce_meeting{id={"demo2", Domain},
                                        metadata=[{"description", "Meeting R&D"},
                                                  {"video", "/test"}],
                                        start_date=utils:now(),
                                        end_date=?NEVER_ENDING_MEETING}),
+
     catch uce_meeting:add(#uce_meeting{id={"agoroom", Domain},
                                        metadata=[{"description", "Meeting agoroom"},
                                                  {"video", "http://encre.2metz.fr/simonsinek_2009x"}],
                                        start_date=1287738533649,
                                        end_date=1287739733649}),
-    
 
-%    ok = feed(Domain),
+    Users = ["thierry.bomandouki@af83.com", "victor.goya@af83.com",
+             "louis.ameline@af83.com", "alexandre.eisenchteter@af83.com",
+             "romain.gauthier@af83.com", "participant"],
 
-    case utils:get(config:get(admin), [uid, auth]) of
-        [Uid, Auth] ->
-            {ok, Sid} = uce_presence:add(#uce_presence{user={Uid, Domain},
-                                                       auth=Auth,
+    lists:foreach(fun(User) ->
+                          catch uce_user:add(#uce_user{id={User, Domain},
+                                                       auth="password",
+                                                       credential="pwd",
                                                        metadata=[]}),
-            io:format("Admin: ~p/~p~n", [Uid, Sid]);
-        Reason ->
-            io:format("No admin account (~p)~n", [Reason])
-    end,
+                          uce_acl:add(#uce_acl{user={User, Domain},
+                                               action="add",
+                                               object="presence"})
+                  end, Users),
+    Hashtags = ["#TED", "#sinek", "#simonsinek", "#ucengine"],
+    lists:foreach(fun(HashTag) ->
+                          uce_event:add(#uce_event{domain=Domain,
+                                                   type="twitter.hashtag.add",
+                                                   location={"demo", Domain},
+                                                   from={"participant", Domain},
+                                                   metadata=[{"hashtag", HashTag}]})
+                  end, Hashtags),
     ok.
-
-
-%% feed(_, []) ->
-%%     ok;
-%% feed(Domain, [Path|Paths]) ->
-%%     ["config", "samples", Meeting, _File] = re:split(Path, "/", [{return, list}]),
-%%     event_helpers:feed(Domain, Path, [{"location", [Meeting]}]),
-%%     feed(Domain, Paths).
-
-%% feed(Domain) ->
-%%     Paths = filelib:wildcard("config/samples/*/*.json"),
-%%     feed(Domain, Paths).
