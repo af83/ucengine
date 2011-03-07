34538721a04be1aedd8f02b5e9e8bdc61623b055
François de Metz <fdemetz@af83.com>
2011-03-07 16:52:23 +0100

    Untabify and reindent uce_async_ws.

--- src/controllers/async/uce_async_ws.erl
+++ src/controllers/async/uce_async_ws.erl
@@ -25,25 +25,25 @@
 
 wait(Location, Search, From, Types, Uid, Start, End, Parent, Socket) ->
     Pid = spawn(fun() ->
-			receive
-			    {ok, WebSocket} ->
-				case uce_async:listen(Location,
-						      Search,
-						      From, 
-						      Types, 
-						      Uid,
-						      Start,
-						      End,
-						      Parent,
-					    Socket) of
-				    ok ->
-					nothing;
-				    {error, Reason} ->
-					?ERROR_MSG("Error in event wait: ~p~n", [Reason])
-				end,
-				yaws_api:stream_process_end(Socket, WebSocket);
-			    _ ->
-				nothing
-			end
-		end),
+                        receive
+                            {ok, WebSocket} ->
+                                case uce_async:listen(Location,
+                                                      Search,
+                                                      From,
+                                                      Types,
+                                                      Uid,
+                                                      Start,
+                                                      End,
+                                                      Parent,
+                                                      Socket) of
+                                    ok ->
+                                        nothing;
+                                    {error, Reason} ->
+                                        ?ERROR_MSG("Error in event wait: ~p~n", [Reason])
+                                end,
+                                yaws_api:stream_process_end(Socket, WebSocket);
+                            _ ->
+                                nothing
+                        end
+                end),
     {websocket, Pid, passive}.
