1c956276e92133aec6809a3f18b4853c4f546c23
Mathieu Lecarme <mathieu.lecarme@af83.com>
2011-05-05 11:31:31 +0200

    Header more error handling.

--- ucengine/src/core/uce_log_h.erl
+++ ucengine/src/core/uce_log_h.erl
@@ -1,3 +1,22 @@
+%%
+%%  U.C.Engine - Unified Colloboration Engine
+%%  Copyright (C) 2011 af83
+%%
+%%  This program is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU Affero General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%
+%%  This program is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU Affero General Public License for more details.
+%%
+%%  You should have received a copy of the GNU Affero General Public License
+%%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%
+
+%% event handler for error_logger
 -module(uce_log_h).
 -author('Mathieu Lecarme mathieu.lecarme@af83.com').
 
@@ -7,15 +26,17 @@ init(_) ->
     {ok, []}.
 terminate(_, State) ->
     State.
-% handle_event({error, Gleader, {Pid, Format, Data}}, _Data2) ->
-%     uce_logger:log(4, error_logger, 0, "[~w ~w] " ++ Format, [Gleader, Pid] ++ Data);
+handle_event({error, Gleader, {Pid, Format, Data}}, State) ->
+     uce_logger:log(4, error_logger, 0, "[~w ~w] " ++ Format, [Gleader, Pid] ++ Data),
+     {ok, State};
 % handle_event({error_report, Gleader, {Pid, std_error, Report}}, _Data) ->
 %     uce_logger:log(4, error_logger, 0, "[~w ~w] ~s" , [Gleader, Pid, fmt_report(Report)]);
     
 % handle_event({error_report, Gleader, {Pid, Type, Report}}, Data) ->
 %     ok;
-% handle_event({warning_msg, Gleader, {Pid, Format, Data}}, Data) ->
-%     ok;
+handle_event({warning_msg, Gleader, {Pid, Format, Data}}, State) ->
+    uce_logger:log(3, error_logger, 0, "[~w ~w] " ++ Format, [Gleader, Pid] ++ Data),
+    {ok, State};
 % handle_event({warning_report, Gleader, {Pid, std_warning, Report}}, Data) ->
 %     ok;
 % handle_event({warning_report, Gleader, {Pid, Type, Report}}, Data) ->
