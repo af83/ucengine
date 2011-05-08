c8b494723c0a8636df657f3d24b82b8fa66c198d
François de Metz <francois@2metz.fr>
2011-05-08 23:57:05 +0200

    Remove some bad stuff in json_helpers.
    Signed-off-by: François de Metz <francois@2metz.fr>

--- ucengine/src/controllers/presence_controller.erl
+++ ucengine/src/controllers/presence_controller.erl
@@ -56,7 +56,7 @@ add(Domain, [], [Name, Credential, Timeout, Metadata], _) ->
                                        location={"", Domain},
                                        type="internal.presence.add"}),
     {Id, _} = User#uce_user.id,
-    json_helpers:created(Domain, {Id, Sid}).
+    json_helpers:json(Domain, 200, {struct, [{uid, Id}, {sid, Sid}]}).
 
 get(Domain, [Id], [], _) ->
     {ok, Record} = uce_presence:get(Domain, {Id, Domain}),
