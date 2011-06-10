22222099f68a907402a83835700b49c01dc9b19e
Victor Goya <victor.goya@af83.com>
2011-06-10 21:57:07 +0200

    bench: update tsung_utils

--- tsung/tsung_utils.erl
+++ tsung/tsung_utils.erl
@@ -4,19 +4,19 @@
 
 extract_last_timestamp({_Pid, DynData}) ->
     {ok, Events} = ts_dynvars:lookup(events, DynData),
-    case Events of
-    undefined ->
-        % An error has occured, retry with the same timestamp
-        case ts_dynvars:lookup(last, DynData) of
-        {ok, LastTimestamp} ->
-            LastTimestamp;
-        % Or the first timestamp
-        false ->
-            0
-        end;
-    Events when is_list(Events) ->
-        {struct, EventAttributes} = lists:last(Events),
-        {<<"datetime">>, Datetime} = lists:keyfind(<<"datetime">>, 1, EventAttributes),
-        integer_to_list(Datetime)
+    if
+        Events == undefined; Events == [] ->
+            % Retry with the same timestamp
+            case ts_dynvars:lookup(last, DynData) of
+                {ok, LastTimestamp} ->
+                    LastTimestamp;
+            % Or the first timestamp
+                false ->
+                    0
+            end;
+        true ->
+            {struct, EventAttributes} = lists:last(Events),
+            {<<"datetime">>, Datetime} = lists:keyfind(<<"datetime">>, 1, EventAttributes),
+            integer_to_list(Datetime)
     end.
 
