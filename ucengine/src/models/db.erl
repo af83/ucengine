9cbf5153782d2abba7f8f921de62fa9e28645786
Mathieu Lecarme <mathieu.lecarme@af83.com>
2011-06-23 15:46:36 +0200

    Counting db:get.

--- ucengine/src/models/db.erl
+++ ucengine/src/models/db.erl
@@ -1,8 +1,10 @@
 -module('db').
 
 -export([get/2]).
+-include("uce.hrl").
 
 get(Module, Domain) ->
+    ?COUNTER(lists:flatten(io_lib:format("db:get:~s", [Module]))),
     case config:get(Domain, db) of
         undefined -> throw({error, no_database});
         _ = Value -> list_to_atom(lists:concat([Module, "_", Value]))
