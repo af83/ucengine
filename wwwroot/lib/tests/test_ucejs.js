860d828f9a81bf775c2f234a93693ac390446ced
François de Metz <fdemetz@af83.com>
2011-08-02 09:49:39 +0200

    Cleanup factories.
    Signed-off-by: François de Metz <fdemetz@af83.com>

--- wwwroot/lib/tests/test_ucejs.js
+++ wwwroot/lib/tests/test_ucejs.js
@@ -8,103 +8,11 @@ module('ucejs', {
 });
 
 var Factories = {
-    createFileEvent: function(params) {
-        params = params || {}
-
-        var metadata = $.extend({}, {id   : 'norris.pdf',
-                                     name : 'norris.pdf'}, params);
-        var eventId = params['eventId'] || "upload_event_id";
-        var from = params['from'] || "test_user";
-
-        return {
-            id: eventId,
-            from: from,
-            type: "internal.file.add",
-            metadata: metadata
-        };
-    },
-    createConversionDoneEvent: function(params) {
-        var metadata = {};
-
-        $(params.pages).each(function(index, page) {
-            metadata[index] = page;
-        });
-
-        return {
-            type: "document.conversion.done",
-            parent: params.parent,
-            metadata: metadata
-        };
-    },
-    createDocumentShareStartEvent: function(params) {
-        var from = params['from'] || "chuck";
-        var metadata = {id: params.id};
-
-        if (params.page) {
-            metadata.page = params.page;
-        }
-        return {
-            type: "document.share.start",
-            from: from,
-            metadata: metadata
-        };
-    },
-    createDocumentShareGotoEvent: function(params) {
-        var from = params['from'] || "chuck";
-        return {
-            type: "document.share.goto",
-            from: from,
-            metadata: {
-                page: params.page
-            }
-        };
-    },
-    createDocumentShareStopEvent: function(params) {
-        var from = params['from'] || "chuck";
-        return {
-            type: "document.share.stop",
-            from: from,
-            metadata: {
-                id: params.id
-            }
-        };
-    },
-    createRosterDeleteEvent: function(params) {
-        var from = params['from'] || "chuck";
-        return {
-            type: "internal.roster.delete",
-            from: from,
-            metadata: {
-                id: params.id
-            }
-        };
-    },
     createPresence: function() {
         return {"user": "myuid", "id": "mysid"};
     },
     getDefaultMeeting: function() {
         return uce.createClient().attachPresence(Factories.createPresence()).meeting("mymeeting");
-    },
-    createStreamNew: function() {
-        return {type: "video.stream.new",
-                metadata : {token : "123456",
-                            channel : "channel_1"}};
-    },
-    createStreamStart: function(broadcaster) {
-        return {type: "video.stream.start",
-                metadata: {broadcaster: broadcaster}}
-    },
-    createStreamStop: function(broadcaster) {
-        return {type: "video.stream.stop",
-                metadata: {broadcaster: broadcaster}}
-    },
-    createMeeting: function(start, end, description) {
-        return {name: "ucemeeting",
-                start_date: start,
-                end_date: end,
-                roster: ["chuck", "bruce"],
-                metadata: {description: description || "test_description",
-                           name: "ucemeeting"}};
     }
 };
 
