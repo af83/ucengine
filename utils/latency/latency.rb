d5be29ee574c7c2f2fa6cb9ae54b3439439af122
Mathieu Lecarme <mathieu.lecarme@af83.com>
2011-08-05 15:55:01 +0200

    Latency: Filters.
     * type
     * types
     * parent
    
    Signed-off-by: Mathieu Lecarme <mathieu.lecarme@af83.com>
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- utils/latency/latency.rb
+++ utils/latency/latency.rb
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 require "em-ucengine"
+require "./lib/score"
 
 # The purpose of this script is to mesure two things:
 #
@@ -23,68 +24,90 @@ require "em-ucengine"
 COUNTER_LIMIT = 5 # Number of runs per batch
 EVENTS_LIMIT = 20 # Maximum number of event sent per batch
 
+$test_number = 0
+
 def latency(filters={})
     filters[:types] ||= 'test.latency'
     filters[:event_generator] ||= Proc.new do |cpt|
         'test.latency'
     end
+    filters[:parent] ||= false
 
     EventMachine::UCEngine.run('localhost', 5280) do |uce|
-        broadcast_scores = {}
-        publishing_scores = {}
+        broadcast_scores = Score.new
+        publishing_scores = Score.new
+        parent_event = nil
 
         uce.connect("participant", "pwd") do |error, session|
-            subcription = session.subscribe("demo", :type => filters[:types]) do |err, events|
-                events.each do |event|
-                    received = Time.new
-                    batch = event['metadata']['batch']
-                    score = (received.to_f * 1000000).to_i - event['metadata']['timer']
+            session.publish("parent_event", "demo") do |err, event_id|
+                parent_event = (filters[:parent]) ? event_id : nil
 
-                    (broadcast_scores[batch] ||= []) << score
+                subcription = session.subscribe("demo", :type => filters[:types], :parent => parent_event) do |err, events|
+                    events.each do |event|
+                        received = Time.new
+                        batch = event['metadata']['batch']
+                        score = (received.to_f * 1000000).to_i - event['metadata']['timer']
+                        broadcast_scores[batch] << score
+                    end
                 end
-            end
 
-            nb_events = 1
-            repeat = 0
-            timer = EM::PeriodicTimer.new(1) do
-                nb_events.times do
-                    sent = Time.new
-                    session.publish(filters[:event_generator].call(repeat), "demo", {
-                        :timer => (sent.to_f * 1000000).to_i,
-                        :batch => nb_events
-                    }) do |err, event_id|
-                        score = ((Time.new - sent) * 1000000).to_i
-                        (publishing_scores[nb_events] ||= []) << score
+                nb_events = 1
+                repeat = 0
+                timer = EM::PeriodicTimer.new(1) do
+                    nb_events.times do |i|
+                        sent = Time.new
+                        event =filters[:event_generator].call(repeat + i, parent_event)
+                        session.publish(event[:type], "demo", {
+                            :timer => (sent.to_f * 1000000).to_i,
+                            :batch => nb_events
+                        }, event[:parent]) do |err, event_id|
+                            score = ((Time.new - sent) * 1000000).to_i
+                            publishing_scores[nb_events] << score
+                            last_event = event_id
+                        end
+
+                        print "."
                     end
 
-                    print "."
-                end
+                    # We reached the event limit, so we stop
+                    if nb_events >= EVENTS_LIMIT
+                        # Waiting for the last events
+                        EM::add_timer 2 do
+                            $test_number += 1
+                            broadcast_scores.sort!
+                            File.open("scores#{$test_number}.csv",'w') do |f|
+                                f.write broadcast_scores.to_csv
+                            end
+                            broadcast_scores.each do |batch, scores|
+                                p "90% of #{batch} events travel in less than #{broadcast_scores.get_9_decile(batch)} µs, min: #{scores[0]} µs, max: #{scores[-1]} µs"
+                            end
 
-                # We reached the event limit, so we stop
-                if nb_events >= EVENTS_LIMIT
-                    timer.cancel
-                    subcription.cancel { EM.stop }
-                    File.open('scores.csv','w') do |f|
-                        broadcast_scores.values.map(&:sort).each_with_index do |score, n|
-                            p "90% of #{n*10} events travel in less than #{score[score.length * 0.9]} µs, min: #{score[0]} µs, max: #{score[-1]} µs"
-                            f.write("#{n*10};#{score[score.length * 0.9]}\n")
+                            puts
+                            publishing_scores.sort!
+                            File.open("publishing#{$test_number}.csv",'w') do |f|
+                                f.write publishing_scores.to_csv
+                            end
+
+                            #publishing_scores.values.map(&:sort).each_with_index do |score, n|
+                            #    p "90% of #{n*10} events were published in less than #{score[score.length * 0.9]} µs, min: #{score[0]} µs, max: #{score[-1]} µs"
+                            timer.cancel
+                            subcription.cancel { EM.stop }
+
+                            #end
+
                         end
                     end
 
-                    publishing_scores.values.map(&:sort).each_with_index do |score, n|
-                        p "90% of #{n*10} events were published in less than #{score[score.length * 0.9]} µs, min: #{score[0]} µs, max: #{score[-1]} µs"
+                    repeat += 1
+
+                    # We reached the end of a batch
+                    if repeat == COUNTER_LIMIT
+                        nb_events += 10
+                        repeat = 0
+                        puts
                     end
+
                 end
-
-                repeat += 1
-
-                # We reached the end of a batch
-                if repeat == COUNTER_LIMIT
-                    nb_events += 10
-                    repeat = 0
-                    puts
-                end
-
             end
         end
     end
@@ -93,24 +116,34 @@ end
 #latency()
 
 ## Test with a single type
-=begin
 latency({
     :types => 'test.latency',
+    :parent => false,
     :event_generator => Proc.new do |cpt|
-        (cpt % 2 == 1 ) ? 'test.latency' : 'test.doomed'
+        { :type => (cpt % 2 == 1 ) ? 'test.latency' : 'test.doomed' }
     end
 })
-=end
 
 ## Test with two types
 latency({
     :types => 'test.latency,test.again',
-    :event_generator => Proc.new do |cpt|
-        ['test.latency', 'test.doomed', 'test.again'][cpt % 3]
+    :event_generator => Proc.new do |cpt, last_event|
+        { :type =>
+            ['test.latency', 'test.doomed', 'test.again'][cpt % 3]
+        }
     end
 })
 
 
 ## Test with parent
+latency({
+    :types => 'test.latency',
+    :event_generator => Proc.new do |cpt, parent|
+        { :type   => 'test.latency',
+          :parent => ((cpt %2) == 0) ? parent : nil
+        }
+    end
+})
+
 
 ## Test with search
