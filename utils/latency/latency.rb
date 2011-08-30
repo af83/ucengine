d647aa50edf44eb6e7134d9f4f6aec2e9d05d5ee
Romain Gauthier <romain.gauthier@af83.com>
2011-08-30 11:54:58 +0200

    Update the latency script.
    Now handle multiple users.
    
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>
    Signed-off-by: Jean-Denis Vauguet <jdvauguet@af83.com>
    Signed-off-by: Mathieu Lecarme <mathieu.lecarme@af83.com>

--- utils/latency/latency.rb
+++ utils/latency/latency.rb
@@ -22,104 +22,135 @@ require "./lib/score"
 # Here we stop because we reached EVENTS_LIMIT
 
 COUNTER_LIMIT = 5 # Number of runs per batch
-EVENTS_LIMIT = 100 # Maximum number of event sent per batch
+EVENTS_LIMIT = 21 # Maximum number of event sent per batch
+NB_CLIENTS = 3 # Number of simultaneous users
 
 $test_number = 0
+$broadcast_scores = Score.new
+$publishing_scores = Score.new
+$finished_users = nil
 
 def latency(filters={})
-    filters[:types] ||= 'test.latency'
-    filters[:event_generator] ||= Proc.new do |cpt|
-        'test.latency'
-    end
-    filters[:parent] ||= false
-
-    EventMachine::UCEngine.run('localhost', 5280) do |uce|
-        broadcast_scores = Score.new
-        publishing_scores = Score.new
-        parent_event = nil
-
-        uce.connect("participant", "pwd") do |error, session|
-            session.publish("parent_event", "demo") do |err, event_id|
-                parent_event = (filters[:parent]) ? event_id : nil
-                params = {
-                    :type => filters[:types],
-                    :parent => parent_event,
-                }
-
-                subcription = session.subscribe("demo", params) do |err, events|
-                    events.each do |event|
-                        received = Time.new
-                        batch = event['metadata']['batch']
-                        score = (received.to_f * 1000000).to_i - event['metadata']['timer']
-                        broadcast_scores[batch] << score
-                    end
-                end
-
-                nb_events = 1
-                repeat = 0
-                timer = EM::PeriodicTimer.new(1) do
-                    nb_events.times do |i|
-                        sent = Time.new
-                        event =filters[:event_generator].call(repeat + i, parent_event)
-                        metadata = {
-                            :timer => (sent.to_f * 1000000).to_i,
-                            :batch => nb_events
-                        }.merge(event[:metadata] || {})
-
-                        session.publish(event[:type], "demo", metadata, event[:parent]) do |err, event_id|
-                            score = ((Time.new - sent) * 1000000).to_i
-                            publishing_scores[nb_events] << score
-                            last_event = event_id
-                        end
-
-                        print "."
-                    end
-
-                    # We reached the event limit, so we stop
-                    if nb_events >= EVENTS_LIMIT
-                        # Waiting for the last events
-                        EM::add_timer 2 do
-                            $test_number += 1
-                            broadcast_scores.sort!
-                            File.open("scores#{$test_number}.csv",'w') do |f|
-                                f.write broadcast_scores.to_all_csv
-                            end
-                            broadcast_scores.each do |batch, scores|
-                                p "90% of #{batch} events travel in less than #{broadcast_scores.get_9_decile(batch)} µs, min: #{scores[0]} µs, max: #{scores[-1]} µs"
-                            end
-
-                            puts
-                            publishing_scores.sort!
-                            File.open("publishing#{$test_number}.csv",'w') do |f|
-                                f.write publishing_scores.to_csv
-                            end
-
-                            #publishing_scores.values.map(&:sort).each_with_index do |score, n|
-                            #    p "90% of #{n*10} events were published in less than #{score[score.length * 0.9]} µs, min: #{score[0]} µs, max: #{score[-1]} µs"
-                            timer.cancel
-                            subcription.cancel { EM.stop }
-
-                            #end
-
-                        end
-                    end
-
-                    repeat += 1
-
-                    # We reached the end of a batch
-                    if repeat == COUNTER_LIMIT
-                        nb_events += 10
-                        repeat = 0
-                        puts
-                    end
-
-                end
+  EventMachine::UCEngine.run('localhost', 5280) do |uce|
+    uce.connect("root", "root") do |error, session|
+      create_user = Proc.new do |i|
+        user = "user#{i}"
+        session.create_user({ :name => user,
+                              :auth => "password",
+                              :credential => "pwd",
+                              :metadata => {}}) do
+          if i > 0
+            i -= 1
+            create_user.call(i)
+          else
+            NB_CLIENTS.times do |i|
+              run_client(user, uce, filters)
             end
+          end
         end
+      end
+
+      $finished_users = NB_CLIENTS
+      create_user.call(NB_CLIENTS)
     end
+  end
 end
 
-#latency()
+def run_client(user, uce, filters)
+  p "run_client"
+
+  filters[:types] ||= 'test.latency'
+  filters[:event_generator] ||= Proc.new do |cpt|
+    'test.latency'
+  end
+  filters[:parent] ||= false
+  parent_event = nil
+
+  uce.connect(user, "pwd") do |error, session|
+    session.publish("parent_event", "demo") do |err, event_id|
+      parent_event = (filters[:parent]) ? event_id : nil
+      params = {
+        :type => filters[:types],
+        :parent => parent_event,
+      }
+
+      subcription = session.subscribe("demo", params) do |err, events|
+        events.each do |event|
+          received = Time.new
+          batch = event['metadata']['batch']
+          score = (received.to_f * 1000000).to_i - event['metadata']['timer']
+          $broadcast_scores[batch] << score
+        end
+      end
+
+      nb_events = 1
+      repeat = 0
+      timer = EM::PeriodicTimer.new(1) do
+        nb_events.times do |i|
+          sent = Time.new
+          event =filters[:event_generator].call(repeat + i, parent_event)
+          metadata = {
+            :timer => (sent.to_f * 1000000).to_i,
+            :batch => nb_events
+          }.merge(event[:metadata] || {})
+
+          session.publish(event[:type], "demo", metadata, event[:parent]) do |err, event_id|
+            score = ((Time.new - sent) * 1000000).to_i
+            $publishing_scores[nb_events] << score
+            last_event = event_id
+          end
+
+          print "."
+        end
+
+        repeat += 1
+
+        p nb_events
+        if nb_events >= EVENTS_LIMIT
+          timer.cancel
+          EM::add_timer 2 do
+            $finished_users -= 1
+            write_results if $finished_users <= 0
+            subcription.cancel { EM.stop }
+          end
+        end
+
+        # We reached the end of a batch
+        if repeat == COUNTER_LIMIT
+          nb_events += 10
+          repeat = 0
+          puts
+        end
+
+      end
+    end
+  end
+end
+
+def write_results
+  p "write_results"
+
+  # Waiting for the last events
+  EM::add_timer 2 do
+    $test_number += 1
+    $broadcast_scores.sort!
+    #File.open("scores#{$test_number}.csv",'w') do |f|
+    #  f.write $broadcast_scores.to_all_csv
+    #end
+    $broadcast_scores.each do |batch, scores|
+      p "90% of #{batch} events travel in less than #{$broadcast_scores.get_9_decile(batch)} µs, min: #{scores[0]} µs, max: #{scores[-1]} µs"
+    end
+
+    puts
+    $publishing_scores.sort!
+    #File.open("publishing#{$test_number}.csv",'w') do |f|
+    #  f.write $publishing_scores.to_csv
+    #end
+
+    # $publishing_scores.values.map(&:sort).each_with_index do |score, n|
+    # p "90% of #{n*10} events were published in less than #{score[score.length * 0.9]} µs, min: #{score[0]} µs, max: #{score[-1]} µs"
+  end
+end
 
 ## Test with a single type
 latency({
